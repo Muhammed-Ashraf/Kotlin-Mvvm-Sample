a03207d384c7caa24226c19015496c05c57c9793
Muhammed_Ashraf <ashraf8534@gmail.com>
2018-12-14 18:10:21 +0530

    inject okhttpand moshi

--- app/src/main/java/com/example/user/kotlin_mvvm_sample/di/AppModule.kt
+++ app/src/main/java/com/example/user/kotlin_mvvm_sample/di/AppModule.kt
@@ -8,10 +8,12 @@ import com.example.user.kotlin_mvvm_sample.data.local.CryptocurrenciesDao
 import com.example.user.kotlin_mvvm_sample.data.local.Database
 import com.example.user.kotlin_mvvm_sample.data.remote.ApiInterface
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -36,15 +38,26 @@ class AppModule {
     fun provideCryptocurrenciesDao(database: Database): CryptocurrenciesDao = database.cryptocurrenciesDao()
 
     @Provides
-    internal fun provideRetrofitInterface(): Retrofit {
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides
+    @Singleton
+    internal fun provideRetrofitInterface(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .build()
     }
 
     @Provides
+    @Singleton
     internal fun provideApi(retrofit: Retrofit): ApiInterface {
         return retrofit.create(ApiInterface::class.java)
     }
